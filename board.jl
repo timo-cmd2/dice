05160a2e179b33e6fc253fca4f40074b54d550c6
Timo S <58859419+timo-cmd@users.noreply.github.com>
2020-10-08 12:23:07 +0200

    add movePiece abillity

--- board.jl
+++ board.jl
@@ -1,5 +1,8 @@
 # create a new board
-# board = makeBoard(10,10,0)
+# board = makeBoard(10, 10, 0)
 function makeBoard(height, width, default_value)
   fill!(zeros(typeof(default_value), height, width), default_value)
 end
+
+# move pieces around
+# movePiece(board, 3, 5, 8, 8)
